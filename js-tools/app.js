01a5e72b30300857ce044fa5121217feadaf3657
Pedro Gomez <gomez.pedro@outlook.com>
2025-09-01 13:51:59 -0700

    mobile navigation

--- js-tools/app.js
+++ js-tools/app.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuButton = document.getElementById('mobile-menu-button');
     const mobileMenu = document.getElementById('mobile-menu');
+    const mobileMenuLinks = document.querySelectorAll('#mobile-menu a');
 
     mobileMenuButton.addEventListener('click', function () {
         mobileMenu.classList.toggle('hidden');
+    });
+
+    mobileMenuLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            mobileMenu.classList.add('hidden');
+        });
     });
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
